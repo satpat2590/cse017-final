086028546d5ccbf94f6397195fcbfb31d2f09517
Satyam Patel <sbp223@lehigh.edu>
2020-08-09 17:22:19 +0000

    Added the insert and delete methods, I'm still implementing the boolean versions of the code however

--- BST.java
+++ BST.java
@@ -1,20 +1,43 @@
+import java.util.Arrays;
+
 class BST {
 
-  int[][] constructor; 
-  int[][] graph; 
+
+int[][] graph; 
+int tempMax; 
 
 //ADJACENCY MATRIX AS THE BINARY TREE
-  public BST(int[][] constructor) {
-    graph = constructor; 
+  BST(int max) {
+    graph = new int[max][max];
+    tempMax = max; 
   }
 
   public void add_link(int from, int to) {
     graph[from][to] = 1; 
+  }
+
+  public boolean find(int item) {
+    for (int x = 0; x < tempMax; x++) {
+      for (int y = 0; y < tempMax; y++) {
+        if (graph[x][y] == 1) {
+          return true; 
+        }
+      }
+    }
+
+    return false; 
+    
   }
   
   public void remove_link(int from, int to) {
     graph[from][to] = 0; 
   }
 
+
+  public void printGraph() {
+    for (int x = 0; x < tempMax; x++) {
+      System.out.println(Arrays.toString(graph[x]));
+    }
+  }
 
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
 
-    int[][] newGraph = new int[10][10]; 
+    Scanner s = new Scanner(System.in); 
+    System.out.println("Input a number that would be the size of the Adjacency Matrix: ");
 
-    BST tree = new BST(newGraph); 
+    int size = s.nextInt();  
+
+    BST tree = new BST(size); 
+
+    tree.add_link(3, 4);
+    tree.add_link(1, 4);
+
+    tree.printGraph();
+
+    System.out.println(tree.find(4)); 
+
 
   }
 }
