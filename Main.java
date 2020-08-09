496c2d52ea1b857adc9c069bccc4dcb255dfb206
Satyam Patel <sbp223@lehigh.edu>
2020-08-09 20:36:11 +0000

    changed formatting and implemented the scanner import for personalized matrix sizes

--- Main.java
+++ Main.java
@@ -6,10 +6,12 @@ class Main {
     Scanner s = new Scanner(System.in); 
     System.out.println("Input a number that would be the size of the Adjacency Matrix: ");
 
+//INPUT A SIZE FOR THE ADJACENCY MATRIX. SIZE WOULD BE A SQUARE, SO THE NUMBER OF ROWS AND COLUMNS WOULD BE THE SAME "SIZE"
     int size = s.nextInt();  
 
     BST tree = new BST(size); 
 
+//TEST FOR THE "INSERT" METHOD 
     tree.printGraph();
     tree.insert(3, 4);
     tree.insert(2, 4);
@@ -19,12 +21,15 @@ class Main {
     tree.insert(5, 7);
     tree.printGraph();
 
+//TEST FOR THE "DELETE" METHOD
     tree.delete(3, 4);
     tree.delete(5, 4);
     tree.printGraph();
 
+//TEST FOR THE "FIND" METHOD 
     System.out.println("\nWas the element found? " + tree.find(8));
 
+//TEST FOR THE "REBALANCE" METHOD 
     tree.rebalance();
     tree.printGraph();
 
